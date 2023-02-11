3ea54253af5a1505eaf2d67baced6295a10401f5
Takumi <ito.tkmi@gmail.com>
2023-02-11 21:08:38 +0900

    20230211210837

--- dotconfig/nvim/lua/gitkumi/remap.lua
+++ dotconfig/nvim/lua/gitkumi/remap.lua
@@ -40,28 +40,3 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 -- File tree
 vim.keymap.set('n', '<leader>b', vim.cmd.NvimTreeFocus)
 vim.keymap.set('n', '<C-b>', vim.cmd.NvimTreeToggle)
-
--- Tabs
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Move to previous/next
--- vim.keymap.set('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
--- vim.keymap.set('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
--- vim.keymap.set('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
--- vim.keymap.set('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-vim.keymap.set('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-vim.keymap.set('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-vim.keymap.set('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-vim.keymap.set('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-vim.keymap.set('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-vim.keymap.set('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-vim.keymap.set('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-vim.keymap.set('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-vim.keymap.set('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-vim.keymap.set('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
-
