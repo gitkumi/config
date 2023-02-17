fbe83091ee03fa56cb67edfddb46173c63c94821
Takumi <ito.tkmi@gmail.com>
2023-02-18 02:56:49 +0900

    20230218025648

--- dotconfig/nvim/lua/gitkumi/set.lua
+++ dotconfig/nvim/lua/gitkumi/set.lua
@@ -10,9 +10,9 @@ vim.opt.number = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -22,7 +22,7 @@ vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.updatetime = 250
+vim.opt.updatetime = 50
 
 vim.opt.undofile = true
 
