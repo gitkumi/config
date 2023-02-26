76fa2b7af942a8956e50dd2ccb22087035e215a7
Takumi <ito.tkmi@gmail.com>
2023-02-27 00:14:28 +0900

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,2 +1,15 @@
-cp -R dotconfig/. ~/.config
-cp -R dotfiles/. ~/
+#!/bin/bash
+
+while getopts "d:" arg; do
+  case $arg in
+    d) declare -r DEVICE=$OPTARG;;
+  esac
+done
+
+if [ -z "$DEVICE" ]; then
+  echo "Device is required."
+  exit 1;
+fi
+
+cp -R $DEVICE/dotconfig/. ~/.config
+cp -R $DEVICE/dotfiles/. ~/
