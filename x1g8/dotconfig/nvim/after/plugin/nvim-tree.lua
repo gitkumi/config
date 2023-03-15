205d85d5ade3c6288d610d53d9850618b11e751d
Takumi <ito.tkmi@gmail.com>
2023-03-15 21:20:39 +0900

    20230315212038

--- x1g8/dotconfig/nvim/after/plugin/nvim-tree.lua
+++ x1g8/dotconfig/nvim/after/plugin/nvim-tree.lua
@@ -7,13 +7,35 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 30,
-    side = 'right'
+    side = 'right',
+    float = {
+      enable = true,
+      quit_on_focus_loss = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * 0.6
+        local window_h = screen_h * 0.6
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
   },
   renderer = {
     group_empty = true,
@@ -28,7 +50,7 @@ require("nvim-tree").setup({
     open_file = {
       quit_on_open = true
     }
-  }
+  },
 })
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { 
