ef6a662bb6cbce4634810498c57bffb79b521681
Takumi <ito.tkmi@gmail.com>
2023-03-25 22:06:40 +0900

    20230325220639

--- x1g8/dotconfig/nvim/lua/gitkumi/remap.lua
+++ x1g8/dotconfig/nvim/lua/gitkumi/remap.lua
@@ -51,7 +51,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>po', builtin.find_files)
 vim.keymap.set('n', '<leader>pp', builtin.git_files)
-vim.keymap.set('n', '<leader>ps', builtin.live_grep)
+vim.keymap.set('n', '<leader>pf', builtin.live_grep)
 vim.keymap.set('n', '<leader>pq', builtin.grep_string)
 vim.keymap.set('n', '<leader>pw', builtin.lsp_references)
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').help_tags)
