6115f39112ae02defc9af703b2166073837c96c0
Takumi <ito.tkmi@gmail.com>
2023-04-13 23:08:01 +0900

    20230413230800

--- x1g8/dotconfig/nvim/lua/gitkumi/set.lua
+++ x1g8/dotconfig/nvim/lua/gitkumi/set.lua
@@ -36,6 +36,8 @@ vim.opt.isfname:append("@-@")
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = 'menuone,noselect'
 
+vim.o.cursorline = true
+
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
