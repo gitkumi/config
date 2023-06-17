fb9ecfd843ac419e023109d7267432802bffb144
Takumi <ito.tkmi@gmail.com>
2023-06-17 19:03:37 +0900

    20230617190337

--- scripts/git_checkout.sh
+++ scripts/git_checkout.sh
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]
   then
-    git checkout "$(git branch | cut -c 3- | gum choose)"
+    git checkout "$(git branch --sort=-committerdate | cut -c 3- | gum choose)"
   else 
     git checkout $1
 fi
