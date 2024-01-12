037923a345ea3a60bd16bb0f5fcf1dee5b51932b
Takumi <ito.tkmi@gmail.com>
2024-01-12 22:15:00 +0900

    20240112221500

--- x1g8/dotconfig/nvim/init.lua
+++ x1g8/dotconfig/nvim/init.lua
@@ -61,25 +61,6 @@ require('lazy').setup({
   'peitalin/vim-jsx-typescript',
 
   {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-      end,
-    },
-  },
-
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
@@ -241,15 +222,6 @@ require('lazy').setup({
         }
       },
     }
-  },
-
-  {
-    'kylechui/nvim-surround',
-    version = '*',
-    event = 'VeryLazy',
-    config = function()
-      require('nvim-surround').setup({})
-    end
   },
 
   {
