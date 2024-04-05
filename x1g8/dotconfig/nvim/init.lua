9b8bc1e6eac37c6ab285a8a23d03cb562cb1656c
Takumi <ito.tkmi@gmail.com>
2024-04-05 21:30:02 +0900

    20240405213002

--- x1g8/dotconfig/nvim/init.lua
+++ x1g8/dotconfig/nvim/init.lua
@@ -168,7 +168,6 @@ require('lazy').setup({
       require('leap').create_default_mappings()
     end
   },
-
   {
     'stevearc/oil.nvim',
     opts = {
@@ -192,108 +191,6 @@ require('lazy').setup({
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-
-  -- {
-  --   'nvim-tree/nvim-tree.lua',
-  --   opts = {
-  --     sort_by = "case_sensitive",
-  --     view = {
-  --       width = 30,
-  --       side = 'right',
-  --       float = {
-  --         enable = true,
-  --         quit_on_focus_loss = true,
-  --         open_win_config = function()
-  --           local screen_w = vim.opt.columns:get()
-  --           local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-  --           local window_w = screen_w * 0.6
-  --           local window_h = screen_h * 0.6
-  --           local window_w_int = math.floor(window_w)
-  --           local window_h_int = math.floor(window_h)
-  --           local center_x = (screen_w - window_w) / 2
-  --           local center_y = ((vim.opt.lines:get() - window_h) / 2)
-  --               - vim.opt.cmdheight:get()
-  --           return {
-  --             border = "rounded",
-  --             relative = "editor",
-  --             row = center_y,
-  --             col = center_x,
-  --             width = window_w_int,
-  --             height = window_h_int,
-  --           }
-  --         end,
-  --       },
-  --     },
-  --     renderer = {
-  --       group_empty = false,
-  --       indent_width = 2,
-  --       indent_markers = {
-  --         enable = true,
-  --         inline_arrows = true,
-  --         icons = {
-  --           corner = "└",
-  --           edge = "│",
-  --           item = "│",
-  --           bottom = "─",
-  --           none = "-",
-  --         },
-  --       },
-  --       icons = {
-  --         webdev_colors = true,
-  --         git_placement = "before",
-  --         modified_placement = "after",
-  --         padding = " ",
-  --         symlink_arrow = " ➛ ",
-  --         show = {
-  --           file = true,
-  --           folder = false,
-  --           folder_arrow = true,
-  --           git = true,
-  --           modified = true,
-  --         },
-  --         glyphs = {
-  --           default = "",
-  --           symlink = "",
-  --           bookmark = "",
-  --           modified = "●",
-  --           folder = {
-  --             arrow_closed = "▶",
-  --             arrow_open = "▼",
-  --             default = "",
-  --             open = "",
-  --             empty = "",
-  --             empty_open = "",
-  --             symlink = "",
-  --             symlink_open = "",
-  --           },
-  --           git = {
-  --             unstaged = "✗",
-  --             staged = "✓",
-  --             unmerged = "~",
-  --             renamed = "➜",
-  --             untracked = "★",
-  --             deleted = "x",
-  --             ignored = "◌",
-  --           },
-  --         },
-  --       },
-  --     },
-  --     git = {
-  --       ignore = false
-  --     },
-  --     filters = {
-  --       dotfiles = false,
-  --     },
-  --     update_focused_file = {
-  --       enable = true,
-  --     },
-  --     actions = {
-  --       open_file = {
-  --         quit_on_open = true
-  --       }
-  --     },
-  --   }
-  -- },
 
   {
     'norcalli/nvim-colorizer.lua',
@@ -405,6 +302,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('telescope').setup {
   defaults = {
     layout_strategy = 'vertical',
+    layout_config = { width = 0.5 },
     mappings = {
       i = {
         ['<C-u>'] = false,
@@ -720,7 +618,6 @@ vim.keymap.set("v", "<leader>r", [["zy:%s@<C-r>z@<C-r>z@g<Left><Left>]])
 vim.keymap.set("v", "<leader>R", [["zy:%s@<C-r>z@<C-r>z@gI<Left><Left><Left>]])
 
 -- File tree
--- vim.keymap.set("n", "<leader>b", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<leader><space>", function()
   require("oil").open()
 end)
