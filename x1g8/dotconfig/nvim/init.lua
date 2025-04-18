32a9399544f5aa74e41f5a47c3a366d2fe9a148d
Takumi Ito <ito.tkmi@gmail.com>
2025-04-18 23:30:31 +0900

    20250418233030

--- x1g8/dotconfig/nvim/init.lua
+++ x1g8/dotconfig/nvim/init.lua
@@ -13,6 +13,39 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.scriptencoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.updatetime = 50
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = "menuone,noselect"
+vim.opt.cursorline = true
+
 require("lazy").setup({
 	{
 		-- LSP Configuration & Plugins
@@ -656,39 +689,6 @@ cmp.setup({
 		{ name = "luasnip" },
 	},
 })
-
-vim.g.mapleader = "\\"
-vim.g.maplocalleader = "\\"
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.scriptencoding = "utf-8"
-vim.opt.termguicolors = true
-vim.opt.showmode = false
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-vim.opt.hidden = true
-vim.opt.number = true
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.updatetime = 50
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.scrolloff = 10
-vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = "menuone,noselect"
-vim.opt.cursorline = true
 
 vim.diagnostic.config({
 	virtual_text = false,
