74badd0f879b5ecd761f5cce9293fda8f7c8475e
Takumi Ito <ito.tkmi@gmail.com>
2026-03-21 21:11:56 +0900

    20260321211155

--- x1g8/dotconfig/nvim/lua/plugins/lsp.lua
+++ x1g8/dotconfig/nvim/lua/plugins/lsp.lua
@@ -21,63 +21,62 @@ return {
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
 		config = function()
-			-- LSP keymaps on_attach function
-			local on_attach = function(_, bufnr)
-				local nmap = function(keys, func, desc)
-					if desc then
-						desc = "LSP: " .. desc
+			-- LSP keymaps via LspAttach autocmd (works regardless of how servers are started)
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp-attach-keymaps", { clear = true }),
+				callback = function(event)
+					local bufnr = event.buf
+					local nmap = function(keys, func, desc)
+						if desc then
+							desc = "LSP: " .. desc
+						end
+						vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 					end
 
-					vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-				end
+					nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+					nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+					nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+					nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+					nmap("gt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
+					nmap("gs", vim.lsp.buf.signature_help)
+					nmap("<leader>re", vim.lsp.buf.rename)
+					nmap("<leader>ca", vim.lsp.buf.code_action)
 
-				nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-				nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-				nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-				nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-				nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-				nmap("gt", vim.lsp.buf.type_definition, "[G]oto [T]ype Definition")
-				nmap("gs", vim.lsp.buf.signature_help)
-				nmap("<leader>re", vim.lsp.buf.rename)
-				nmap("<leader>ca", vim.lsp.buf.code_action)
-
-				-- Create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
-			end
-
-			-- Language servers to install and configure
-			local servers = {
-				lua_ls = {
-					Lua = {
-						workspace = { checkThirdParty = false },
-						telemetry = { enable = false },
-					},
-				},
-			}
+					-- Create a command `:Format` local to the LSP buffer
+					vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+						vim.lsp.buf.format()
+					end, { desc = "Format current buffer with LSP" })
+				end,
+			})
 
 			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-			-- Ensure the servers above are installed
-			local mason_lspconfig = require("mason-lspconfig")
+			-- Set shared defaults for all LSP servers
+			vim.lsp.config("*", {
+				capabilities = capabilities,
+			})
 
-			mason_lspconfig.setup({
-				ensure_installed = vim.tbl_keys(servers),
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-							on_attach = on_attach,
-							settings = servers[server_name],
-							filetypes = (servers[server_name] or {}).filetypes,
-						})
-					end,
+			-- Configure individual servers
+			vim.lsp.config("lua_ls", {
+				settings = {
+					Lua = {
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
+					},
 				},
 			})
+
+			-- Ensure servers are installed via mason
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+
+			-- Enable servers
+			vim.lsp.enable("lua_ls")
 
 			-- Diagnostic configuration
 			vim.diagnostic.config({
